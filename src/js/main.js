ff24fbea99254f485abf275887fe48b1b616d4fe
Dunmy <dunmyyoung@gmail.com>
2025-07-09 14:32:32 +0100

    wdd ass

---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+import ProductData from './ProductData.mjs';
+const dataSource = new ProductData('tents');
+const listElement = document.querySelector('.product-list');
+const tentList = new ProductList('tents', dataSource, listElement);
+tentList.init();
